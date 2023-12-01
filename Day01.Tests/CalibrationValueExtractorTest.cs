09199c77eb869206b5d0df4feeb44d1e05642dc9
Kris Hoogendoorn <kris@hgndrn.nl>
2023-12-01 11:04:01 +0100

    Day 01 - Star 2

--- Day01.Tests/CalibrationValueExtractorTest.cs
+++ Day01.Tests/CalibrationValueExtractorTest.cs
@@ -2,32 +2,17 @@ namespace Day01.Tests
 {
     public class CalibrationValueExtractorTest
     {
-        [Test]
-        public void Test1()
+        [TestCase("two1nine", ExpectedResult = 29)]
+        [TestCase("eightwothree", ExpectedResult = 83)]
+        [TestCase("abcone2threexyz", ExpectedResult = 13)]
+        [TestCase("xtwone3four", ExpectedResult = 24)]
+        [TestCase("4nineeightseven2", ExpectedResult = 42)]
+        [TestCase("zoneight234", ExpectedResult = 14)]
+        [TestCase("7pqrstsixteen", ExpectedResult = 76)]
+        public int Test(string line)
         {
             var x = new CalibrationValueExtractor();
-            Assert.That(x.ParseLine("1abc2"), Is.EqualTo(12));
-        }
-
-        [Test]
-        public void Test2()
-        {
-            var x = new CalibrationValueExtractor();
-            Assert.That(x.ParseLine("pqr3stu8vwx"), Is.EqualTo(38));
-        }
-
-        [Test]
-        public void Test3()
-        {
-            var x = new CalibrationValueExtractor();
-            Assert.That(x.ParseLine("a1b2c3d4e5f"), Is.EqualTo(15));
-        }
-
-        [Test]
-        public void Test4()
-        {
-            var x = new CalibrationValueExtractor();
-            Assert.That(x.ParseLine("treb7uchet"), Is.EqualTo(77));
+            return x.ParseLine(line);
         }
     }
 }
--- Day01/CalibrationValueExtractor.cs
+++ Day01/CalibrationValueExtractor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Day01
 {
@@ -10,20 +6,56 @@ namespace Day01
     {
         public int ParseLine(string line)
         {
-            var charArray = line.ToCharArray();
             var builder = new StringBuilder();
-            builder.Append(FirstNumber(charArray));
-            builder.Append(FirstNumber(charArray.Reverse()));
+            builder.Append(FirstNumber(line));
+            builder.Append(LastNumber(line));
 
             return int.Parse(builder.ToString());
         }
 
-        private char FirstNumber(IEnumerable<char> array)
+        private char FirstNumber(string line)
         {
-            foreach (char c in array)
+            while (line.Length > 0)
             {
-                if (char.IsNumber(c))
-                    return c;
+                switch (line)
+                {
+                    case var x when x.StartsWith("zero"): return '0';
+                    case var x when x.StartsWith("one"): return '1';
+                    case var x when x.StartsWith("two"): return '2';
+                    case var x when x.StartsWith("three"): return '3';
+                    case var x when x.StartsWith("four"): return '4';
+                    case var x when x.StartsWith("five"): return '5';
+                    case var x when x.StartsWith("six"): return '6';
+                    case var x when x.StartsWith("seven"): return '7';
+                    case var x when x.StartsWith("eight"): return '8';
+                    case var x when x.StartsWith("nine"): return '9';
+                    case var x when char.IsNumber(x[0]): return x[0];
+                    default: line = line[1..]; break;
+                }
+            }
+
+            return '0';
+        }
+
+        private char LastNumber(string line)
+        {
+            while (line.Length > 0)
+            {
+                switch (line)
+                {
+                    case var x when x.EndsWith("zero"): return '0';
+                    case var x when x.EndsWith("one"): return '1';
+                    case var x when x.EndsWith("two"): return '2';
+                    case var x when x.EndsWith("three"): return '3';
+                    case var x when x.EndsWith("four"): return '4';
+                    case var x when x.EndsWith("five"): return '5';
+                    case var x when x.EndsWith("six"): return '6';
+                    case var x when x.EndsWith("seven"): return '7';
+                    case var x when x.EndsWith("eight"): return '8';
+                    case var x when x.EndsWith("nine"): return '9';
+                    case var x when char.IsNumber(x[^1]): return x[^1];
+                    default: line = line[..^1]; break;
+                }
             }
 
             return '0';
