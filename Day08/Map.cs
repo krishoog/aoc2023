4d9d1a9e7c44878e2628d328ac496edd4a398ec5
Kris Hoogendoorn <kris@hgndrn.nl>
2023-12-14 23:23:08 +0100

    Day 08 - Star 2

--- Day08/Map.cs
+++ Day08/Map.cs
@@ -4,13 +4,28 @@ namespace Day08
 {
     public class Map
     {
-        private readonly record struct Node(string left, string right);
+        private class Node
+        {
+            public Node(string label)
+            {
+                Label = label;
+            }
 
-        private readonly Dictionary<string, Node> nodes = [];
-        private const string firstNode = "AAA";
-        private const string lastNode = "ZZZ";
-        private const string multipathFirstNodeEnding = "A";
-        private const string multipathLastNodeEnding = "Z";
+            public string Label { get; init; }
+            public Node? Left { get; set; }
+            public Node? Right { get; set; }
+
+            public Node NextNode(char direction)
+            {
+                return direction == 'R' ? Right : Left;
+            }
+        }
+
+        private readonly Dictionary<string, (string, string)> nodes = [];
+        private const string firstNodeLabel = "AAA";
+        private const string lastNodeLabel = "ZZZ";
+        private const string multipathFirstNodeLabelEnding = "A";
+        private const string multipathLastNodeLabelEnding = "Z";
         private string? directions;
 
         public void AddDirections(string directions)
@@ -28,55 +43,84 @@ namespace Day08
 
         public void AddNode(string name, string left, string right)
         {
-            nodes[name] = new Node(left, right);
+            nodes[name] = (left, right);
         }
 
         public int Traverse()
         {
-            var currentNode = firstNode;
-            var steps = 0;
-            while (currentNode != lastNode)
-            {
-                if (directions[steps % directions.Length] == 'R')
-                {
-                    currentNode = nodes[currentNode].right;
-                }
-                else
-                {
-                    currentNode = nodes[currentNode].left;
-                }
+            var node = BuildGraph(firstNodeLabel);
 
+            return CountSteps(node, lastNodeLabel);
+        }
+
+        public long MultipathTraverse()
+        {
+            var graphs = nodes.Where(x => x.Key.EndsWith(multipathFirstNodeLabelEnding)).Select(x => BuildGraph(x.Key));
+            var steps = graphs.Select(x => (long)CountSteps(x, multipathLastNodeLabelEnding));
+
+            return LCM(steps);
+        }
+
+        private int CountSteps(Node node, string label)
+        {
+            var steps = 0;
+            while (!node.Label.EndsWith(label))
+            {
+                node = node.NextNode(directions[steps % directions.Length]);
                 steps++;
             }
 
             return steps;
         }
 
-        public int MultipathTraverse()
+        private Node BuildGraph(string rootNodeLabel)
         {
-            var currentNodes = nodes.Where(x => x.Key.EndsWith(multipathFirstNodeEnding)).Select(x => x.Key).ToArray();
-            var steps = 0;
-            while (!currentNodes.All(x => x.EndsWith(multipathLastNodeEnding)))
+            var stack = new Stack<Node>();
+            var repository = new Dictionary<string, Node>();
+            var root = CreateNode(rootNodeLabel, stack, repository);
+            while (stack.Count > 0)
             {
-                if (directions[steps % directions.Length] == 'R')
-                {
-                    for (int i = 0; i < currentNodes.Length; i++)
-                    {
-                        currentNodes[i] = nodes[currentNodes[i]].right;
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < currentNodes.Length; i++)
-                    {
-                        currentNodes[i] = nodes[currentNodes[i]].left;
-                    }
-                }
+                var current = stack.Pop();
 
-                steps++;
+                current.Left = FindOrCreateNode(nodes[current.Label].Item1, stack, repository);
+                current.Right = FindOrCreateNode(nodes[current.Label].Item2, stack, repository);
             }
 
-            return steps;
+            return root;
+        }
+
+        private Node FindOrCreateNode(string label, Stack<Node> stack, Dictionary<string, Node> repository)
+        {
+            if (!repository.TryGetValue(label, out Node? node))
+            {
+                node = CreateNode(label, stack, repository);
+            }
+
+            return node;
+        }
+
+        private static Node CreateNode(string label, Stack<Node> stack, Dictionary<string, Node> repository)
+        {
+            var node = new Node(label);
+
+            repository[label] = node;
+            stack.Push(node);
+
+            return node;
+        }
+
+        // https://stackoverflow.com/a/29717490
+        static long LCM(IEnumerable<long> numbers)
+        {
+            return numbers.Aggregate(lcm);
+        }
+        static long lcm(long a, long b)
+        {
+            return Math.Abs(a * b) / GCD(a, b);
+        }
+        static long GCD(long a, long b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
         }
     }
 }
